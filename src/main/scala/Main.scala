54dee47638150efe9a3b3b2269aca76b01a330fa
KazuyaMiayshita <k_miyashita@itandi.co.jp>
2019-04-30 13:36:16 +0900

    選択したページの画像のURLの一覧を取得する。
    * URLにはマルチバイト文字が利用可能
    * XPathの指定は `//img[contains(@class, 'pict')]` のような方法も可能
    * XPathの指定で `//img/@src` はうまくいかないようだ。
      * XPathではWebElement(or List<WebElement>)を取得し、その後getAttributeでStringにするのが良さそう。型も守れる

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,23 @@
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
 import com.machinepublishers.jbrowserdriver.Timezone
 import com.machinepublishers.jbrowserdriver.JBrowserDriver
 import com.machinepublishers.jbrowserdriver.Settings
 
+
+
 object Main extends App {
   // You can optionally pass a Settings object here,
   // constructed using Settings.Builder
-  val driver: JBrowserDriver = new JBrowserDriver(Settings.builder().
-    timezone(Timezone.ASIA_TOKYO).build())
+  val driver: JBrowserDriver = new JBrowserDriver(Settings.builder()
+    .timezone(Timezone.ASIA_TOKYO).build())
+
+  print("> ")
+  val url = readLine
 
   // This will block for the page load and any
   // associated AJAX requests
-  driver.get("http://example.com")
+  driver.get(url)
 
   // You can get status code unlike other Selenium drivers.
   // It blocks for AJAX requests and page loads after clicks 
@@ -21,6 +27,13 @@ object Main extends App {
   // Returns the page source in its current state, including
   // any DOM updates that occurred after page load
   System.out.println(driver.getPageSource())
+
+  val imgsrcs: List[String] = {
+    import collection.JavaConverters._
+    driver.findElementsByXPath("//img").asScala.map(_.getAttribute("src")).toList
+  }
+
+  imgsrcs foreach println
   
   // Close the browser. Allows this thread to terminate.
   driver.quit()
